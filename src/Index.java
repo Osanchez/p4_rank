0da0b1fcd7786ebc0c5fd927426b3410855b7bd7
osanchez <osanchez@umass.edu>
2018-04-07 18:56:08 -0400

    bm25 complete

--- src/Index.java
+++ src/Index.java
@@ -1,9 +1,13 @@
+// Resource below was used to create method for sorting hashmaps by value
+// http://www.java67.com/2015/01/how-to-sort-hashmap-in-java-based-on.html
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.*;
 
 public class Index {
@@ -14,6 +18,7 @@ public class Index {
     private double k1_value;
     private double k2_value;
     private double b_value;
+    private double mu;
 
     private Index() {
         dataStorage = new HashMap<>();
@@ -22,13 +27,20 @@ public class Index {
         k1_value = 1.2;
         k2_value = 100.0;
         b_value = 0.75;
+        mu = 1500;
     }
 
     public static void main(String[] args) throws IOException, ParseException {
         Index index = new Index();
         HashMap dataStorage = index.readJSON("shakespeare-scenes.json");
-        //index.documentToString();
-        index.allScenesToString();
+        //index.calculateBM25("Q1","the king queen royalty");
+        index.calculateBM25("Q2","servant guard soldier");
+        //index.calculateBM25("Q3","hope dream sleep");
+        //index.calculateBM25("Q4","ghost spirit");
+        //index.calculateBM25("Q5","fool jester player");
+        //index.calculateBM25("Q6","to be or not to be");
+
+        //index.sceneTextToString();
     }
 
     private HashMap readJSON(String fileName) throws ParseException, IOException {
@@ -84,10 +96,10 @@ public class Index {
         return dataStorage;
     }
 
-    private double calculateBM25(String query) {
+    private void calculateBM25(String queryLabel, String query) {
+        HashMap<String, Double> calculatedResults = new HashMap<>();
 
-        double result = 0.00;
-        String[] split_query = query.split("");
+        String[] split_query = query.split(" ");
 
         //no relevance information, we can set r and R to 0, which would give a pi value of 0.5
         double R = 0.00; //number of relevant documents for the query
@@ -97,7 +109,6 @@ public class Index {
         double k_2 = k2_value; //set empirically
 
 
-        //TODO: calculate these values
         //gets the frequency of each word in the query and stores it to avoid having to loop every word in the query each time
         HashMap<String, Integer> frequencyOfTerm_Q = new HashMap<>();
         for(String word: split_query) {
@@ -115,10 +126,9 @@ public class Index {
             }
         }
 
-        //gets the average number of documents
-        //average document length
+        //gets the average document length
         double avdl = 0.00; //average length of all documents
-        double numberDocuments = allScenes.size();
+        int numberDocuments = allScenes.size();
 
         for(String key : allScenes.keySet()) {
             avdl += allScenes.get(key).size();
@@ -126,180 +136,87 @@ public class Index {
         }
         avdl = avdl/numberDocuments;
 
-        //BM25 ranking //TODO: frequency of term in current document.
-        double N = allScenes.size(); //number of documents in the collection (Scene is a document)
-        double n_i; //number of documents containing term i
-        double dl; //current document size (Words in the document maybe?)
-        double f_i = 0; //frequency of term in current document
+        //BM25 ranking
+        int N = allScenes.size(); //number of documents in the collection (Scene is a document)
+        int n_i; //number of documents containing term i
+        double qf_i; //frequency of term i in query
+        int f_i = 0; //frequency of term i in current document
+        int dl;
 
-        for(String key : allScenes.keySet()) {
-            dl = allScenes.get(key).size(); //size of current document
-            for (int i = 0; i < split_query.length; i++) {
-                double qf_i = frequencyOfTerm_Q.get(split_query[i]); //frequency of term i in the document
+        for(String key : allScenes.keySet()) { //for every scene
+            dl = allScenes.get(key).size(); //size of current document (Scene)
+            double result = 0.00;
+            for (String queryWord : split_query) { //for all words in the query
+                qf_i = frequencyOfTerm_Q.get(queryWord); //frequency of term i in the document
                 double K = k_1 * ((1 - b_value) + b_value * (dl / avdl));
-                HashMap<String, ArrayList<Integer>> possibleScenes = dataStorage.get(split_query[i]);
-                n_i = possibleScenes.size();
-                System.out.println(Math.log(((r_i + 0.5) / (R - r_i + 0.5) / ((n_i - r_i + 0.5) / (N - n_i - R + r_i + 0.5))) * (((k_1 + 1) * f_i) / (K + f_i)) * (((k_2 + 1) * qf_i) / (k_2 + qf_i))));
-            }
-        }
+                HashMap<String, ArrayList<Integer>> queryScenes = dataStorage.get(queryWord);
+                n_i = queryScenes.size(); //size;
 
-        return result;
-
-    }
-
-    private void writeTermFiles(String fileName, HashMap data)
-            throws IOException {
-        HashMap<String, ArrayList<String>> copyData = data;
-        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-        ArrayList<String> alphabeticallySorted = new ArrayList<>();
-        for (String entry : copyData.keySet()) {
-            ArrayList<String> dataEntries = copyData.get(entry);
-            for (String dataentry : dataEntries) {
-                if (!alphabeticallySorted.contains(dataentry)) {
-                    alphabeticallySorted.add(dataentry);
-                }
-            }
-        }
-        Collections.sort(alphabeticallySorted);
-        for (String entry : alphabeticallySorted) {
-            writer.write(entry);
-            writer.newLine();
-        }
-        writer.close();
-    }
-
-    private HashMap<String, ArrayList<String>> usedMoreThan(String[] words, String moreThanWord) { //scenes
-        HashMap<String, ArrayList<String>> result = new HashMap<>();
-
-        HashMap<String, HashMap<String, Integer>> wordCounts = new HashMap<>(); //words
-        HashMap<String, Integer> morethanWordCounts = new HashMap<>();
-
-        //word count for each scene moreThanWord appears in
-        HashMap<String, ArrayList<Integer>> moreThanWordScenes = dataStorage.get(moreThanWord);
-        for (String scene : moreThanWordScenes.keySet()) {
-            morethanWordCounts.put(scene, moreThanWordScenes.get(scene).size());
-        }
-
-        //word count for each scene each word appears in
-        for (String word : words) {
-            HashMap<String, ArrayList<Integer>> wordScenes = dataStorage.get(word);
-            HashMap<String, Integer> sceneWordCount = new HashMap<>();
-            for (String scene : wordScenes.keySet()) {
-                int wordCountScene = wordScenes.get(scene).size();
-                sceneWordCount.put(scene, wordCountScene);
-            }
-            wordCounts.put(word, sceneWordCount);
-        }
-
-        //get union of words for each scene
-        HashMap<String, Integer> unionScenes = new HashMap<>();
-        for (String word : wordCounts.keySet()) {
-            HashMap<String, Integer> scenes = wordCounts.get(word);
-            for (String scene : scenes.keySet()) {
-                if (unionScenes.containsKey(scene)) {
-                    int current = unionScenes.get(scene);
-                    int add = scenes.get(scene);
-                    unionScenes.replace(scene, current + add);
-                } else {
-                    unionScenes.put(scene, scenes.get(scene));
-                }
-            }
-        }
-
-        //comparison for all scenes of other word
-        ArrayList<String> finalScenes = new ArrayList<>();
-        for (String scene : morethanWordCounts.keySet()) {
-            try {
-                int wordCountWords = unionScenes.get(scene);
-                int wordCountMoreThanWord = morethanWordCounts.get(scene);
-                if (wordCountWords > wordCountMoreThanWord) {
-                    finalScenes.add(scene);
-                }
-            } catch (NullPointerException e) { //if the moreThanWord does not appear at all
-                int wordCountWords = 0;
-                int wordCountMoreThanWord = morethanWordCounts.get(scene);
-                if (wordCountWords > wordCountMoreThanWord) {
-                    finalScenes.add(scene);
-                }
-            }
-        }
-
-        //comparison for all scenes of words
-        for (String scene : unionScenes.keySet()) {
-            try {
-                int wordCountWords = unionScenes.get(scene);
-                int wordCountMoreThanWord = morethanWordCounts.get(scene);
-                if (wordCountWords > wordCountMoreThanWord) {
-                    finalScenes.add(scene);
-                }
-            } catch (NullPointerException e) { //if the moreThanWord does not appear at all
-                int wordCountWords = unionScenes.get(scene);
-                int wordCountMoreThanWord = 0;
-                if (wordCountWords > wordCountMoreThanWord) {
-                    finalScenes.add(scene);
-                }
-            }
-        }
-
-        result.put("Result", finalScenes);
-        return result;
-    }
-
-    private HashMap isMentioned(String[] mentionedWord, String scene_play) { //scenes or play
-        HashMap<String, ArrayList<String>> result = new HashMap<>();
-
-        for (String word : mentionedWord) {
-            ArrayList<String> scenes = new ArrayList<>();
-            ArrayList<String> plays = new ArrayList<>();
-            if (dataStorage.containsKey(word.toLowerCase())) {
-                HashMap<String, ArrayList<Integer>> termValues = dataStorage.get(word.toLowerCase());
-                for (String scene : termValues.keySet()) {
-                    String play = scene.split(":")[0];
-                    if (!plays.contains(play)) {
-                        plays.add(play);
-                    }
-                    if (!scenes.contains(scene)) {
-                        scenes.add(scene);
+                //frequency of term in current document
+                String[] currentDocumentText = sceneText.get(key);
+                for (String word : currentDocumentText) {
+                    if (word.equals(queryWord)) {
+                        f_i++;
                     }
                 }
-                if (scene_play.equals("s")) {
-                    result.put(word, scenes);
-                } else if (scene_play.equals("p")) {
-                    result.put(word, plays);
-                }
-            }
-        }
-        return result;
-    }
+                //calculate BM25 Score
+                double var1 = ((r_i + 0.5)/(R - r_i + 0.5))/((n_i - r_i + 0.5)/(N - n_i - R + r_i + 0.5));
+                var1 = Math.log(var1);
+                double var2 = ((k_1 + 1) * f_i)/(K + f_i);
+                double var3 = ((k_2 + 1) * qf_i)/(k_2 + qf_i);
 
-    private HashMap phraseMentioned(String phrase) { //scenes
-        HashMap<String, ArrayList<String>> result = new HashMap<>();
-        ArrayList<String> scenes = new ArrayList<>();
-        String[] splitPhrase = phrase.split(" ");
-        int wordsInPhrase = splitPhrase.length;
-        boolean phraseInScene;
+                double resultingVar;
 
-        //scenes that contain the first word
-        HashMap<String, ArrayList<Integer>> possibleScenes = dataStorage.get(splitPhrase[0]); //all scenes with first word
-        for (String scene : possibleScenes.keySet()) { //for all scenes that have the first word
-            ArrayList<Integer> wordIndex = possibleScenes.get(scene); //all indexes of first word in scene
-            for (Integer index : wordIndex) { //for each index of first word
-                phraseInScene = true; //by default start with true
-                String[] text = sceneText.get(scene); //get the text for comparison
-                for (int x = 0; x < wordsInPhrase; x++) { //iterate every word in phrase
-                    if (!splitPhrase[x].equals(text[index + x])) { //check if next text word matches next phrase word
-                        phraseInScene = false;
-                        break;
-                    }
-                }
-                if (phraseInScene) {
-                    scenes.add(scene);
-                    break;
-                }
+
+                if (var2 == 0)
+                    resultingVar = var1 * var3;
+                else if (var3 == 0)
+                    resultingVar = var1 * var2;
+                else
+                    resultingVar = var1 * var2 * var3;
+
+                result += resultingVar;
+
             }
+            calculatedResults.put(key, result);
         }
-        result.put(phrase, scenes);
-        return result;
+
+        Comparator<Map.Entry<String, Double>> valueComparator = (o1, o2) -> {
+            if(o1.getValue() > o2.getValue()) {
+                return -1;
+            }
+            if(o1.getValue() < o2.getValue()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        };
+
+        Set<Map.Entry<String, Double>> entries = calculatedResults.entrySet();
+
+        List<Map.Entry<String, Double>> listOfEntries = new ArrayList<>(entries);
+
+        Collections.sort(listOfEntries, valueComparator);
+
+        //new hash map of sorted values
+        LinkedHashMap<String, Double> sortedByValue = new LinkedHashMap<>(listOfEntries.size());
+
+        //add values back to hash map
+        for(Map.Entry<String, Double> entry : listOfEntries){
+            sortedByValue.put(entry.getKey(), entry.getValue());
+        }
+
+        int sceneRank = 1;
+
+        //TODO: optional column formatting
+        DecimalFormat dec = new DecimalFormat("#0.000");
+        for(Map.Entry entry : sortedByValue.entrySet()) {
+            String scene = entry.getKey().toString();
+            Double rankValue = (Double) entry.getValue();
+            System.out.println(queryLabel + " " + scene + "\t\t\t\t\t" + sceneRank + " " + dec.format(rankValue) + " Osanchez-bm25");
+            sceneRank++;
+        }
+
     }
 
     public void documentToString() {
@@ -309,8 +226,15 @@ public class Index {
     }
 
     public void allScenesToString() {
-        for(Map.Entry entry : allScenes.entrySet()) {
+        for (Map.Entry entry : allScenes.entrySet()) {
             System.out.println(entry);
+        }
+    }
+
+    public void sceneTextToString() {
+        for(Map.Entry entry : sceneText.entrySet()) {
+            System.out.print(entry.getKey() + ": ");
+            System.out.println(Arrays.toString((String[]) entry.getValue()));
         }
     }
 }
